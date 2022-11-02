4d30ef99494f58968b61f17a6af790519faa6231
Antoine Coulon <antoine@30c27b.com>
2022-11-02 20:17:30 +0100

    added a few tests

---
+++ bqueue_test.go
@@ -0,0 +1,41 @@
+package bqueue_test
+
+import (
+	"path/filepath"
+	"testing"
+
+	"pkg.coulon.dev/bqueue"
+)
+
+func TestNewStore(t *testing.T) {
+	d := t.TempDir()
+	f := filepath.Join(d, "queue.db")
+	s, err := bqueue.NewStore(f, 0600, nil)
+	if err != nil {
+		t.Fatal(err)
+	} else if s == nil {
+		t.Fatal("unexpected nil")
+	}
+	s.Close()
+}
+
+func TestNewQueue(t *testing.T) {
+	d := t.TempDir()
+	f := filepath.Join(d, "queue.db")
+	s, err := bqueue.NewStore(f, 0600, nil)
+	if err != nil {
+		t.Fatal(err)
+	} else if s == nil {
+		t.Fatal("unexpected nil for store")
+	}
+	defer s.Close()
+
+	q, err := s.NewQueue([]byte("foobar"))
+	if err != nil {
+		t.Fatal(err)
+	} else if q == nil {
+		t.Fatal("unexpected nil for queue")
+	}
+}
+
+func
